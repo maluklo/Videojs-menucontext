08e58a034919367c5fd00d3399d3b5e882d91608
Marcos Luciano Kloczko <mmaluklo@gmail.com>
2017-11-21 18:42:55 -0200

    Add files via upload

---
+++ menucontext.js
@@ -0,0 +1,178 @@
+﻿(function(window, vjs){
+var registerPlugin = videojs.registerPlugin || videojs.plugin;
+'use strict';
+var vjs_merge = function(obj1, obj2){
+    if (!obj2) { return obj1; }
+    for (var key in obj2){
+       if (Object.prototype.hasOwnProperty.call(obj2, key)) {
+           obj1[key] = obj2[key];
+      }
+}
+    return obj1;
+};
+var Menu = vjs.getComponent('Menu');
+vjs.registerComponent('PopupMenu', vjs.extend(Menu, {
+    className: 'vjs-rightclick-popup',
+    popped: false,
+    constructor: function(player, options){
+        Menu.call(this, player, options);
+        var player_ = player;
+        this.addClass(this.className);
+        this.hide();
+        var _this = this;
+        var opt = this.options_;
+        var offset = opt.offset||5;
+        this.addChild(new MenuItemLink(player, {
+           
+           // modificar
+           href: 'https://github.com/maluklo', 
+           label: 'Powered by Maluklo',
+            
+        
+        }));
+        player_.on('contextmenu', function(evt){
+            evt.preventDefault();
+            if (_this.popped)
+            {
+                _this.hide();
+                _this.popped = false;
+            }
+            else
+            {
+                _this.show();
+                var oX = evt.offsetX;
+                var oY = evt.offsetY;
+                var left_shift =
+                    _this.el_.offsetWidth+oX+offset-player_.el_.offsetWidth;
+                left_shift = Math.max(0, left_shift);
+                var top_shift =
+                    _this.el_.offsetHeight+oY+offset-player_.el_.offsetHeight;
+                top_shift = Math.max(0, top_shift);
+                oX = oX-left_shift;
+                oY = oY-top_shift;
+                _this.el_.style.top=oY+'px';
+                _this.el_.style.left=oX+'px';
+                _this.popped = true;
+            }
+        });
+        player_.on('click', function(evt){
+            if (_this.popped)
+            {
+                _this.hide();
+                _this.popped = false;
+                evt.stopPropagation();
+                evt.preventDefault();
+                return false;
+            }
+        });
+        this.children().forEach(function(item){
+            item.on('click', function(evt){
+                _this.hide();
+                _this.popped = false;
+            });
+        });
+    }
+}));
+var Component = vjs.getComponent('Component');
+vjs.registerComponent('Component', vjs.extend(Component, {
+    createEl: function(type, props){
+        var custom_class = this.options_['class'];
+        custom_class = custom_class ? ' '+custom_class : '';
+        var proto_component = Component.prototype;
+        var container = proto_component.createEl.call(this, 'div', vjs_merge({
+         className: custom_class,
+        }, props));
+        this.createContent(container);
+        return container;
+    },
+    createContent: function(container){},
+}));
+var MenuItem = vjs.getComponent('MenuItem');
+vjs.registerComponent('MenuItemLink', vjs.extend(MenuItem, {
+    createEl: function(type, props){
+        var prot = MenuItem.prototype;
+        var label = this.localize(this.options_['label']);
+        var el = prot.createEl.call(this, 'li', vjs_merge({
+            className: 'vjs-menu-item vjs-menu-item-link',
+            innerHTML: '',
+        }, props));
+        this.link = Component.prototype.createEl('a', {
+            className: 'vjs-menu-link',
+            innerHTML: label,
+        }, {
+            target: '_blank',
+            href: this.options_.href||'#',
+        });
+        el.appendChild(this.link);
+        return el;
+    },
+    handleClick: function(e){ e.stopPropagation(); },
+}));
+var MenuItemLink = vjs.getComponent('MenuItemLink');
+vjs.registerComponent('MenuItemLink', vjs.extend(MenuItem, {
+    constructor: function(player, options){
+        MenuItem.call(this, player, options);
+        var player_ = player;
+        this.on('click', function(){
+            this.selected(false);
+        });
+    }
+}));
+function add_css(url, ver){
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', url+(ver ? '?'+ver : ''));
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+function get_class_name(element){
+    return element.className.split(/\s+/g);
+}
+function add_class_name(element, class_name){
+    var classes = get_class_name(element);
+    if (classes.indexOf(class_name)==-1)
+    {
+        classes.push(class_name);
+        element.className = classes.join(' ');
+        return true;
+    }
+    return false;
+}
+var ContextmenuSkin = function(video, opt){
+    var _this = this;
+    this.vjs = video;
+    this.el = video.el();
+    this.opt = opt;
+    this.classes_added = [];
+    this.vjs.on('dispose', function(){ _this.dispose(); });
+    this.apply();
+
+};
+ContextmenuSkin.prototype.apply = function(){
+    var c, classes = [this.opt.className];
+    if (this.opt.show_controls_before_start)
+        classes.push('vjs-show-controls-before-start');
+    while ((c = classes.shift()))
+    {
+        if (add_class_name(this.el, c))
+            this.classes_added.push(c);
+    }
+};
+var defaults = {
+className: 'Contextmenu',
+    
+};
+vjs.plugin('menucontext', function(options){
+    var opt = vjs.mergeOptions(defaults, options);
+    if (opt.css && (!options.className || options.css))
+    add_css(opt.css, opt.ver);
+    new ContextmenuSkin(this, opt);
+    var video = this;
+    opt = vjs.mergeOptions({}, opt);
+    video.ready(function(){
+    video.addChild('PopupMenu', vjs.mergeOptions({}, opt));
+    });
+});
+}(window, window.videojs));
+
+
+
